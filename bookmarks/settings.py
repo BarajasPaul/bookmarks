303d17514bcab5c04e6ba083bdd8152342db2d7e
Paul Barajas <paul.barajas@linux.com>
2020-12-12 09:24:21 -0600

    adding detail view for images

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'easy_thumbnails',
     'django_extensions',
     'social_django',
 ]
