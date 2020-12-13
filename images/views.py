0ca870134b70092ded207801dde4ad829f83e775
Paul Barajas <paul.barajas@linux.com>
2020-12-13 12:31:56 -0600

    Adding AJAX pagination to your list views

--- images/views.py
+++ images/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
 
 from common.decorators import ajax_required
 from .forms import ImageCreateForm
@@ -11,10 +13,34 @@ from .models import Image
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request,
-                 'images/image/detail.html',
-                 {'section': 'images',
-                  'image': image})
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
 
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                   {'section': 'images', 'images': images})
 
 @ajax_required
 @login_required
@@ -29,11 +55,10 @@ def image_like(request):
                 image.users_like.add(request.user)
             else:
                 image.users_like.remove(request.user)
-            return  JsonResponse({'statu':'ok'})
+            return JsonResponse({'status':'ok'})
         except:
             pass
     return JsonResponse({'status':'error'})
-
 
 @login_required
 def image_create(request):
